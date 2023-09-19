325c36488d385bd5f3dbe8af230495655a5fe4b6
lelesagma <132224036+lelesagma@users.noreply.github.com>
2023-09-19 18:50:51 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+class WebBrowser(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Web Browser')
+        self.setGeometry(100, 100, 800, 600)
+
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+
+        self.browser = QWebEngineView()
+        layout.addWidget(self.browser)
+
+        # Charger une page web (par exemple, google.com)
+        self.browser.setUrl('https://www.google.com')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    browser = WebBrowser()
+    browser.show()
+    sys.exit(app.exec())
